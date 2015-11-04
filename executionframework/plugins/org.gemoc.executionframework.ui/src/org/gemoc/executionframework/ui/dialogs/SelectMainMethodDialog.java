2524d3592912eb5543e4d0f5c1390850a5487c0d
Fabien Coulon <fabien.coulon@inria.fr>
2015-11-04 11:55:45 +0100

    Add a SelectDialog for methods from Aspects tagged @main

---
+++ executionframework/plugins/org.gemoc.executionframework.ui/src/org/gemoc/executionframework/ui/dialogs/SelectMainMethodDialog.java
@@ -0,0 +1,72 @@
+package org.gemoc.executionframework.ui.dialogs;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.viewers.ILabelProvider;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.dialogs.ElementListSelectionDialog;
+
+import fr.inria.diverse.k3.al.annotationprocessor.Aspect;
+import fr.inria.diverse.k3.al.annotationprocessor.Main;
+
+
+public class SelectMainMethodDialog extends ElementListSelectionDialog  {
+
+	private Set<Class> _aspects;
+
+	public SelectMainMethodDialog(Set<Class> aspects, EObject modelElem, ILabelProvider renderer) {
+		super(PlatformUI.getWorkbench().getActiveWorkbenchWindow().getShell(), renderer);
+		_aspects = aspects;
+		update(modelElem);
+	}
+
+	/**
+	 * Display only methods with @main from Aspects applied on modelElem
+	 */
+	public void update(EObject modelElem){
+		Class target = modelElem.eClass().getClass();
+		List<Method> selection = new ArrayList<Method>();
+		
+		for (Class asp : _aspects)
+		{
+			if(getTarget(asp) == target)
+			{
+				for(Method m : asp.getMethods())
+				{
+					if(isAnnotedMain(m))
+					{
+						selection.add(m);
+					}
+				}
+			}
+		}
+		
+		this.setElements(selection.toArray());
+	}
+
+	/**
+	 * Return true is their is @Main
+	 */
+	private boolean isAnnotedMain(Method m){
+		return m.isAnnotationPresent(fr.inria.diverse.k3.al.annotationprocessor.Main.class);
+	}
+
+	/**
+	 * Return the targeted class from the @Aspect
+	 */
+	private Class getTarget(Class aspect){
+		
+		Annotation annotation = aspect.getAnnotation(fr.inria.diverse.k3.al.annotationprocessor.Aspect.class);
+		Aspect k3tag = (Aspect) annotation;
+		return k3tag.className();
+	}
+
+}
+
